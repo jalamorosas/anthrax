8a29a7371a5e881205abbc94e3bcaf303e0abc86
jvalamo <jalamorosas@gmail.com>
2018-08-01 11:35:23 -0400

    fade in

--- scripts.js
+++ scripts.js
@@ -11,7 +11,29 @@ $(document).ready(function(){
     $('body').css('display', 'none');
     $('body').fadeIn(1000);
     $('span').css('display', 'none');
-    $('span').fadeIn(3500);
-    $('p').css('display', 'none');
-    $('p').fadeIn(3000);
+    $('span').fadeIn(3000);
+
     });
+
+$(document).on("scroll", function () {
+    /*calculates how far down page you are*/
+    var pageTop = $(document).scrollTop()
+
+    /*adds height of browser window to pageTop*/
+    var pageBottom = pageTop + $(window).height()
+
+    /*saves tags into variable*/
+    var tags = $("section")
+    
+    /* loop to check if tag is in area to fade in*/
+    for (var i = 0; i < tags.length; i++) {
+        /*gets each item in the tags variable*/
+        var tag = tags[i]
+        /*if the tag is further up than the bottom of the window it will show it*/
+        if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible")
+        } else {
+        $(tag).removeClass("visible")
+        }
+}
+})
