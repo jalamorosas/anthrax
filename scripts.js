d22e418d4fbc7e2e3b19fbcb45943aaa8cdf5334
jvalamo <jalamorosas@gmail.com>
2018-08-01 10:47:20 -0400

    added jquery

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ window.onscroll = () => {
     if(this.scrollY <= 10) nav.className = ''; 
     else nav.className  = 'scroll';
 };
+
+
+$(document).ready(function(){
+
+    /*! Fades in page on load */
+    $('body').css('display', 'none');
+    $('body').fadeIn(1000);
+    $('span').css('display', 'none');
+    $('span').fadeIn(3500);
+    $('p').css('display', 'none');
+    $('p').fadeIn(3000);
+    });
