ab728886a8c4a7d83cf4931d53e4a2f122f04d0e
jvalamo <jalamorosas@gmail.com>
2018-08-02 01:02:05 -0400

    semi-final draft

--- scripts.js
+++ scripts.js
@@ -17,31 +17,8 @@ $(document).ready(function(){
 
     });
 
-$(document).on("scroll", function () {
-    /*calculates how far down page you are*/
-    var pageTop = $(document).scrollTop()
-
-    /*adds height of browser window to pageTop*/
-    var pageBottom = pageTop + $(window).height()
-
-    /*saves tags into variable*/
-    var tags = $("section")
-    
-    /* loop to check if tag is in area to fade in*/
-    for (var i = 0; i < tags.length; i++) {
-        /*gets each item in the tags variable*/
-        var tag = tags[i]
-        /*if the tag is further up than the bottom of the window it will show it*/
-        if ($(tag).position().top < pageBottom) {
-        $(tag).addClass("visible")
-        } else {
-        $(tag).removeClass("visible")
-        }
-}
-})
-
 // Changing the defaults
 window.sr = ScrollReveal({ reset: true });
 
 // Customizing a reveal set
-sr.reveal('.reveal', { duration: 450, delay: 2,});
+sr.reveal('.reveal', { duration: 500, delay: 2,});
